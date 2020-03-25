90ce2ec616aa61f22965898b32a1eca1b434ca48
Yilmaz <bingolyilmaz@hotmail.com>
2020-03-25 16:03:58 -0400

    styling react-modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,16 @@ const OptionModal = props => {
       isOpen={props.selectedOption && true}
       contentLabel="Selected Option"
       onRequestClose={props.closeModal}
+      closeTimeoutMS={300}
+      className="modal"
     >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={props.closeModal}>OK</button>
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && (
+        <p className="modal__body">{props.selectedOption}</p>
+      )}
+      <button className="button" onClick={props.closeModal}>
+        OK
+      </button>
     </Modal>
   );
 };
